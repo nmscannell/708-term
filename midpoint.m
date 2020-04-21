f1acd61dd4a23aef4e0177a9d328217a5ba86f54
Leuenberger <leuenbe4@uwm.edu>
2020-04-21 14:04:41 -0500

    Adding scripts for midpoint and simpsons

---
+++ midpoint.m
@@ -0,0 +1,6 @@
+% Midpoint Rule Integration
+%   Calculates the integral of f(x)=2+sin(2(sqrt(x)) over
+%   the interval [1,6] using Midpoint Rule 
+%   
+% Input: array of N values
+% Output: array of results for each N value
---
+++ simpsons.m
@@ -0,0 +1,6 @@
+% Simpson's Rule Integration
+%   Calculates the integral of f(x)=2+sin(2(sqrt(x)) over
+%   the interval [1,6] using Simpson's Rule 
+%   
+% Input: array of N values
+% Output: array of results for each N value
