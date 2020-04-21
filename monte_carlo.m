a744eef7c74a765db108691b3e02d4d78824160f
nmscannell <45504902+nmscannell@users.noreply.github.com>
2020-04-21 18:48:20 -0500

    initial commit
    Skeletons for Monte Carlo and Trapezoid Rule integrations

---
+++ monte_carlo.m
@@ -0,0 +1,10 @@
+% Monte Carlo Integration
+%   Calculates the integral of f(x)=2+sin(2(sqrt(x)) over
+%   the interval [1,6] using Monte Carlo Simulation 
+%   
+% Input: array of N values
+% Output: array of results for each N value
+
+function results = monte_carlo(n_values)
+
+end
---
+++ trapezoid.m
@@ -0,0 +1,10 @@
+% Trapezoid Rule Integration
+%   Calculates the integral of f(x)=2+sin(2(sqrt(x)) over
+%   the interval [1,6] using Trapezoid Rule 
+%   
+% Input: array of N values
+% Output: array of results for each N value
+
+function results = trapezoid(n_values)
+
+end
