fc2ad3ee2fea1287c963ae12e5cd5b75422bd78f
nmscannell <45504902+nmscannell@users.noreply.github.com>
2020-04-21 20:47:46 -0500

    Finished Trapezoid?
    Quick implementation of trapezoid rule. Will review it.

--- trapezoid.m
+++ trapezoid.m
@@ -3,8 +3,23 @@
 %   the interval [1,6] using Trapezoid Rule 
 %   
 % Input: array of N values
-% Output: array of results for each N value
+% Output: array of results for each N values
 
 function results = trapezoid(n_values)
+    results = zeros(size(n_values));
+    for i=1:length(n_values)
+        h = 5/n_values(i);
+        sum = 0;
+        for k=1:n_values(i)-1
+            x = 1 + k * h;
+            sum = sum + f(x);
+        end
+        results(i) = sum * 2 + f(1) + f(6);
+        results(i) = results(i) * h/2;
+    end
+end
 
+function out = f(x)
+    out = 2;
+    out = out + sin(2*sqrt(x));
 end
