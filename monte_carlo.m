d6cb3380e25162ce7fce4a801f9a4768685fdb1d
nmscannell <45504902+nmscannell@users.noreply.github.com>
2020-04-21 21:11:00 -0500

    Maybe finished trap/monte?
    Whipped up trapezoid rule integration and monte carlo simulation. Will review.

--- monte_carlo.m
+++ monte_carlo.m
@@ -1,10 +1,23 @@
 % Monte Carlo Integration
 %   Calculates the integral of f(x)=2+sin(2(sqrt(x)) over
-%   the interval [1,6] using Monte Carlo Simulation 
+%   the interval [1,6] using Monte Carlo Simulation. For each
+%   N value, the average is calculated for the function evaluated
+%   at N random points within the interval [1,6]. The average is
+%   then multiplied by the length of the interval. As N increases,
+%   the accuracy of Monte Carlo increases.
 %   
 % Input: array of N values
 % Output: array of results for each N value
 
 function results = monte_carlo(n_values)
-
+    results = zeros(size(n_values));
+    for i=1:length(n_values)
+        sum = 0;
+        for j=1:n_values(i)
+            x = 5*rand + 1;
+            sum = sum + f(x);
+        end
+        sum = sum/n_values(i);
+        results(i) = sum * 5;
+    end
 end
